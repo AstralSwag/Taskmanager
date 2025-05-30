1910321d5e369fb6c5054d2db642e9e9967ff6d7
astralswag <astralswag@gmail.com>
2025-05-30 04:28:38 +0300

    fix

--- init-tables.sh
+++ init-tables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-PGPASSWORD="$DB_PASSWORD" psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<EOSQL
+PGPASSWORD="$DB_PASSWORD" psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<'EOSQL'
     -- Создаем таблицу states
     CREATE TABLE IF NOT EXISTS states (
         id UUID PRIMARY KEY,
